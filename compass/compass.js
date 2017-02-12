f0a6b52a7e65aca8d1c2d6f26baa2ef57fa98b10
3marproof <omar0star@yahoo.com>
2017-02-12 23:54:46 +0200

    Added Compass Resource
    here is a demo
    
    https://youtu.be/ahGx6qvGHnc

---
+++ compass/compass.js
@@ -0,0 +1,53 @@
+﻿API.onUpdate.connect(function (sender) {
+
+    if (0.3 < API.getGameplayCamDir().X && 0.3 < API.getGameplayCamDir().Y) {
+
+        API.drawText("NE", 350, 1000, 1, 255, 255, 255, 255, 2, 1, false, true, 0);
+
+    }
+
+    else if (API.getGameplayCamDir().X<-0.3 && 0.3 < API.getGameplayCamDir().Y) {
+
+        API.drawText("NW", 350, 1000, 1, 255, 255, 255, 255, 2, 1, false, true, 0);
+
+    }
+
+    else if (0.3 < API.getGameplayCamDir().X && API.getGameplayCamDir().Y < -0.3) {
+
+        API.drawText("SE", 350, 1000, 1, 255, 255, 255, 255, 2, 1, false, true, 0);
+
+    }
+
+    else if (API.getGameplayCamDir().X < -0.3 && API.getGameplayCamDir().Y < -0.3) {
+
+        API.drawText("SW", 350, 1000, 1, 255, 255, 255, 255, 2, 1, false, true, 0);
+
+    }
+
+    else if (-0.3 < API.getGameplayCamDir().X && API.getGameplayCamDir().X < 0.3 && API.getGameplayCamDir().Y < -0.3) {
+
+        API.drawText("S", 350, 1000, 1, 255, 255, 255, 255, 2, 1, false, true, 0);
+
+    }
+
+    else if (API.getGameplayCamDir().X < -0.3 && -0.3 < API.getGameplayCamDir().Y && API.getGameplayCamDir().Y < 0.3) {
+
+        API.drawText("W", 350, 1000, 1, 255, 255, 255, 255, 2, 1, false, true, 0);
+
+    }
+
+    else if (0.3 < API.getGameplayCamDir().X && -0.3 < API.getGameplayCamDir().Y && API.getGameplayCamDir().Y < 0.3) {
+
+        API.drawText("E", 350, 1000, 1, 255, 255, 255, 255, 2, 1, false, true, 0);
+
+    }
+
+    else if (-0.3 < API.getGameplayCamDir().X && API.getGameplayCamDir().X < 0.3 && API.getGameplayCamDir().Y > 0.3) {
+
+        API.drawText("N", 350, 1000, 1, 255, 255, 255, 255, 2, 1, false, true, 0);
+
+    }
+
+});
+
+
