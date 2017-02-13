f71c510cd516df00f912cf4ccc4d2dcd32c34541
3marproof <omar0star@yahoo.com>
2017-02-14 01:02:16 +0200

    Fixed

--- compass/compass.js
+++ compass/compass.js
@@ -1,48 +1,51 @@
 ﻿API.onUpdate.connect(function (sender) {
 
-    if (0.3 < API.getGameplayCamDir().X && 0.3 < API.getGameplayCamDir().Y) {
+    var x = API.getGameplayCamDir().X;
+    var y = API.getGameplayCamDir().Y;
+
+    if (0.3 < x && 0.3 < y) {
 
         API.drawText("NE", 350, 1000, 1, 255, 255, 255, 255, 2, 1, false, true, 0);
 
     }
 
-    else if (API.getGameplayCamDir().X<-0.3 && 0.3 < API.getGameplayCamDir().Y) {
+    else if (x<-0.3 && 0.3 < y) {
 
         API.drawText("NW", 350, 1000, 1, 255, 255, 255, 255, 2, 1, false, true, 0);
 
     }
 
-    else if (0.3 < API.getGameplayCamDir().X && API.getGameplayCamDir().Y < -0.3) {
+    else if (0.3 < x && y < -0.3) {
 
         API.drawText("SE", 350, 1000, 1, 255, 255, 255, 255, 2, 1, false, true, 0);
 
     }
 
-    else if (API.getGameplayCamDir().X < -0.3 && API.getGameplayCamDir().Y < -0.3) {
+    else if (x < -0.3 && y < -0.3) {
 
         API.drawText("SW", 350, 1000, 1, 255, 255, 255, 255, 2, 1, false, true, 0);
 
     }
 
-    else if (-0.3 < API.getGameplayCamDir().X && API.getGameplayCamDir().X < 0.3 && API.getGameplayCamDir().Y < -0.3) {
+    else if (-0.3 < x && x < 0.3 && y < -0.3) {
 
         API.drawText("S", 350, 1000, 1, 255, 255, 255, 255, 2, 1, false, true, 0);
 
     }
 
-    else if (API.getGameplayCamDir().X < -0.3 && -0.3 < API.getGameplayCamDir().Y && API.getGameplayCamDir().Y < 0.3) {
+    else if (x < -0.3 && -0.3 < y && y < 0.3) {
 
         API.drawText("W", 350, 1000, 1, 255, 255, 255, 255, 2, 1, false, true, 0);
 
     }
 
-    else if (0.3 < API.getGameplayCamDir().X && -0.3 < API.getGameplayCamDir().Y && API.getGameplayCamDir().Y < 0.3) {
+    else if (0.3 < x && -0.3 < y && y < 0.3) {
 
         API.drawText("E", 350, 1000, 1, 255, 255, 255, 255, 2, 1, false, true, 0);
 
     }
 
-    else if (-0.3 < API.getGameplayCamDir().X && API.getGameplayCamDir().X < 0.3 && API.getGameplayCamDir().Y > 0.3) {
+    else if (-0.3 < x && x < 0.3 && y > 0.3) {
 
         API.drawText("N", 350, 1000, 1, 255, 255, 255, 255, 2, 1, false, true, 0);
 
